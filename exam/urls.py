ec53a6718564afa0b31077d68245ea4cef5b214a
ayush jaiswal <118955455+whoknowayush@users.noreply.github.com>
2025-10-31 11:28:16 +0530

    urls.py

--- exam/urls.py
+++ exam/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.quiz_view, name='quiz'),
     path('submit/', views.submit_quiz, name='submit_quiz'),
+    path('auto-submit-exam/', views.auto_submit_exam, name='auto_submit_exam'),
+    path('exam-submitted/', views.exam_submitted, name='exam_submitted'),
 ]
+]
+
